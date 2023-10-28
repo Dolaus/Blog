5acc8ab42ee3589b96198c33d257308a4ce9cc31
Dolaus <dsadasdasdas19@gmail.com>
2023-10-28 17:11:11 +0300

    Test

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,8 @@ Route::get('/', function () {
 Route::get('/qq', function () {
     return 333;
 });
+Route::get('/q1', function () {
+    return 2;
+});Route::get('/q2', function () {
+    return 1;
+});
